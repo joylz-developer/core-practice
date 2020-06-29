b309ddf8b2c90a16ee56724f741a89f6fed3885a
joylz-developer <48956617+joylz-developer@users.noreply.github.com>
2020-06-29 17:00:47 +0300

    Добавьте файлы проекта.

---
+++ core-practice/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace core_practice.Models {
+  public class ErrorViewModel {
+    public string RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+  }
+}
